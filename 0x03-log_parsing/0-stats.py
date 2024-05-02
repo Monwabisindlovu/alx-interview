d4e58fa99a0ac49ec86ebc33ce41f5ff058ba69d
monwabisindlovu <monwabisindlovu78@gmail.com>
2024-05-02 06:16:01 +0000

    0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,32 +1,55 @@
 #!/usr/bin/python3
+"""0-stats.py
 
-import sys
-import signal
-import re
+Reads stdin line by line and computes metrics based on the input format:
+    <status code> <file size>
+Prints the total file size and counts of possible status codes in the format:
+    File size: <total size>
+    <status code>: <number>
+"""
 
-total_size = 0
-status_codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
-count = 0
+import fileinput
 
-def signal_handler(sig, frame):
-    print(f"File size: {total_size}")
-    for code in sorted(status_codes.keys()):
-        if status_codes[code] > 0:
-            print(f"{code}: {status_codes[code]}")
-    sys.exit(0)
+def print_logs(file_size: int, status_codes: dict):
+    """Prints file size and status code counts.
+    
+    Args:
+        file_size (int): Total file size.
+        status_codes (dict): Dictionary of status codes and their counts.
+        
+    Returns:
+        None
+    """
+    print("File size:", file_size)
+    for code, count in sorted(status_codes.items()):
+        if count > 0:
+            print(f"{code}: {count}")
 
-signal.signal(signal.SIGINT, signal_handler)
+def parse_log():
+    """Parses logs from stdin and computes metrics."""
+    file_size = 0
+    status_codes = {
+        "200": 0, "301": 0, "400": 0, "401": 0,
+        "403": 0, "404": 0, "405": 0, "500": 0
+    }
+    current_line = 0
 
-for line in sys.stdin:
-    count += 1
-    match = re.match(r"^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) - \[(.*?)\] \"GET /projects/260 HTTP/1.1\" (\d{3}) (\d+)$", line)
-    if match:
-        ip, date, status_code, file_size = match.groups()
-        total_size += int(file_size)
-        status_codes[int(status_code)] += 1
-    if count == 10:
-        print(f"File size: {total_size}")
-        for code in sorted(status_codes.keys()):
-            if status_codes[code] > 0:
-                print(f"{code}: {status_codes[code]}")
-        count = 0
+    try:
+        for line in fileinput.input():
+            data = line.split()
+            if len(data) < 2:
+                continue
+            file_size += int(data[-1])
+            status = data[-2]
+            if status in status_codes:
+                status_codes[status] += 1
+            current_line += 1
+            if current_line % 10 == 0:
+                print_logs(file_size, status_codes)
+    except KeyboardInterrupt:
+        pass
+    print_logs(file_size, status_codes)
+
+if __name__ == "__main__":
+    parse_log()
+
