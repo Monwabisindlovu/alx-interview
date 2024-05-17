96b1c27c46d7e7e0721ad839be1efe7c9a2a0723
monwabisindlovu <monwabisindlovu78@gmail.com>
2024-05-17 09:38:57 +0000

    0-nqueens.py

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,95 +1,49 @@
 #!/usr/bin/python3
-"""N Queens problem solver"""
-
 import sys
 
-def is_safe(board, row, col, N):
-    """
-    Check if it's safe to place a queen at board[row][col]
-    """
-    # Check this row on the left side
-    for i in range(col):
-        if board[row][i] == 1:
-            return False
-
-    # Check upper diagonal on left side
-    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
-        if board[i][j] == 1:
-            return False
-
-    # Check lower diagonal on left side
-    for i, j in zip(range(row, N), range(col, -1, -1)):
-        if board[i][j] == 1:
-            return False
-
-    return True
-
-def solve(board, col, N):
-    """
-    Recursive function to solve N Queens problem
-    """
-    # Base case: If all queens are placed
-    if col >= N:
+def solve_n_queens(n):
+    def can_place(pos, ocuppied_positions):
+        for i in range(len(ocuppied_positions)):
+            if ocuppied_positions[i] == pos or \
+                ocuppied_positions[i] - i == pos - len(ocuppied_positions) or \
+                ocuppied_positions[i] + i == pos + len(ocuppied_positions):
+                return False
         return True
 
-    # Consider this column and try placing this queen in all rows one by one
-    for i in range(N):
-        if is_safe(board, i, col, N):
-            # Place this queen in board[i][col]
-            board[i][col] = 1
+    def place_queen(n, index, ocuppied_positions, all_ocuppied_positions):
+        if index == n:
+            all_ocuppied_positions.append(ocuppied_positions[:])
+            return
 
-            # Recur to place rest of the queens
-            if solve(board, col + 1, N):
-                return True
+        for i in range(n):
+            if can_place(i, ocuppied_positions):
+                ocuppied_positions.append(i)
+                place_queen(n, index + 1, ocuppied_positions, all_ocuppied_positions)
+                ocuppied_positions.pop()
 
-            # If placing queen in board[i][col] doesn't lead to a solution then remove queen from board[i][col]
-            board[i][col] = 0
-
-    # If the queen can't be placed in any row in this column col then return False
-    return False
-
-def print_solution(board):
-    """
-    Print the solution
-    """
-    N = len(board)
-    for i in range(N):
-        for j in range(N):
-            print("[{}, {}]".format(i, j), end=" ") if board[i][j] == 1 else print("_", end=" ")
-        print()
+    all_ocuppied_positions = []
+    place_queen(n, 0, [], all_ocuppied_positions)
+    return all_ocuppied_positions
 
 def main():
-    """
-    Main function
-    """
-    # Check the number of arguments
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
         sys.exit(1)
 
-    # Parse the argument as integer
     try:
-        N = int(sys.argv[1])
+        n = int(sys.argv[1])
     except ValueError:
         print("N must be a number")
         sys.exit(1)
 
-    # Check if N is at least 4
-    if N < 4:
+    if n < 4:
         print("N must be at least 4")
         sys.exit(1)
 
-    # Initialize the chess board
-    board = [[0 for _ in range(N)] for _ in range(N)]
-
-    # Solve the N Queens problem
-    if not solve(board, 0, N):
-        print("No solution found")
-        sys.exit(1)
-
-    # Print the solution
-    print_solution(board)
+    all_ocuppied_positions = solve_n_queens(n)
+    for ocuppied_positions in all_ocuppied_positions:
+        solution = [[i, pos] for i, pos in enumerate(ocuppied_positions)]
+        print(solution)
 
 if __name__ == "__main__":
     main()
-
