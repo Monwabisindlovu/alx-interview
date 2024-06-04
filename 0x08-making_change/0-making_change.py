6e80833c4d6140b34aa3afc2e61386bfd4f160af
monwabisi ndlovu <monwabisindlovu78@gmail.com>
2024-06-04 11:42:16 +0000

    0-making_change.py

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,17 +1,23 @@
 #!/usr/bin/python3
-
-""" 0-making_change.py """
+"""
+This module contains the makeChange function to solve fewest coins errors
+"""
 
 
 def makeChange(coins, total):
+    """
+    Return fewest number of coins needed to meet a given amount total
+    """
     if total <= 0:
         return 0
-
-    dp = [float('inf')] * (total + 1)
-    dp[0] = 0
-
-    for coin in coins:
-        for amount in range(coin, total + 1):
-            dp[amount] = min(dp[amount], dp[amount - coin] + 1)
-
-    return dp[total] if dp[total] != float('inf') else -1
+    elif len(coins) == 0:
+        return -1
+    coins.sort()
+    sum = n_coins = 0
+    i = len(coins) - 1
+    while sum < total:
+        while i > 0 and sum + coins[i] > total:
+            i -= 1
+        sum += coins[i]
+        n_coins += 1
+    return n_coins if sum == total else -1
